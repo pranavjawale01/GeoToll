0596f3977d6381c06ce40c725a2fedbe7b7b833c
OmkarAher45687 <aher45687@gmail.com>
2024-10-03 23:04:37 +0530

    DashBoard/Profile created

--- WebApp/map-tracker/src/App.js
+++ WebApp/map-tracker/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import ProfileForm from "./components/ProfileForm";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<ProfileForm />} />
       </Routes>
     </Router>
   );
