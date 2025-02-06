56ae39e0637579c7e7755373c4d4660de9680e92
pranavjawale01 <jawalepranav01@gmail.com>
2025-02-07 01:49:58 +0530

    Multi Vehicle Selction and Penalty Features Added

--- Location Tracker App/app/src/main/java/com/example/locationtrackerapp/HelperFunctions/DailyResetWorker.kt
+++ Location Tracker App/app/src/main/java/com/example/locationtrackerapp/HelperFunctions/DailyResetWorker.kt
@@ -5,21 +5,22 @@ import com.example.locationtrackerapp.HelperFunctions.FirebaseHelper
 import com.example.locationtrackerapp.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 
-class DailyResetWorker (context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+class DailyResetWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
         val user = FirebaseAuth.getInstance().currentUser ?: return Result.failure()
 
         val todayTotalDistanceInput = inputData.getDouble("todayTotalDistance", 0.0)
         val todayTotalHighwayDistanceInput = inputData.getDouble("todayTotalHighwayDistance", 0.0)
+        val currentVehicleId = inputData.getString("currentVehicleId") ?: return Result.failure()  // ✅ Added parameter
 
-        FirebaseHelper.getTodayTotalDistance(user.uid) { todayTotalDistance ->
-            FirebaseHelper.getTodayTotalHighwayDistance(user.uid) { todayTotalHighwayDistance ->
+        FirebaseHelper.getTodayTotalDistance(user.uid, currentVehicleId) { todayTotalDistance ->
+            FirebaseHelper.getTodayTotalHighwayDistance(user.uid, currentVehicleId) { todayTotalHighwayDistance ->
 
                 val updatedTotalDistance = (todayTotalDistance ?: 0.0) + todayTotalDistanceInput
                 val updatedTotalHighwayDistance = (todayTotalHighwayDistance ?: 0.0) + todayTotalHighwayDistanceInput
 
-                FirebaseHelper.saveTodayTotalDistance(user.uid, updatedTotalDistance)
-                FirebaseHelper.saveTodayTotalHighwayDistance(user.uid, updatedTotalHighwayDistance)
+                FirebaseHelper.saveTodayTotalDistance(user.uid, currentVehicleId, updatedTotalDistance)
+                FirebaseHelper.saveTodayTotalHighwayDistance(user.uid, currentVehicleId, updatedTotalHighwayDistance)
 
                 resetValues()
             }
